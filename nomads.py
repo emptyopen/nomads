dc81d32daa8e9f4c59f4db4794353ba955152150
Matthew Takao <takaomatt@gmail.com>
2018-06-17 21:15:25 -0700

    setting up architecture

--- nomads.py
+++ nomads.py
@@ -22,3 +22,69 @@ class Player(object):
 
     def change_character():
         pass
+
+
+class Game:
+
+# ----------------------------------------------------------------------------------------------------------------------------------- PREP
+
+    def __init__(self):
+
+        # window
+        self.caption = "The Nomads"
+        self.fps = 60
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1800, 717))  # (890, 503) , 214 vert for black, 20 margin
+        self.background = pygame.image.load('pics/n-wall.png')
+        self.one_off = True # toggler that ensures screen updates after state update?
+
+
+
+
+# -------------------------------------------------------------------------------------------------------------------- EXECUTE
+
+    def main(self):
+
+        # setup
+        pygame.init()
+        pygame.display.set_caption(self.caption)
+        self.map_components()
+        self.place_components()
+        if not DEBUG:
+            self.create_basetime()
+
+        while True: # main game loop
+
+            self.update_state()
+
+            # update state of game continuously
+            for event in pygame.event.get():
+
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit
+
+            self.update_display()
+
+
+    def update_state(self):
+
+        pass
+
+
+# --------------------------------------------------------------------------------------------------------- DRAW
+
+    def update_display(self):
+
+        self.screen.blit(self.background, (self.p_offset[0], self.p_offset[1]))
+
+
+    # blacksword.tff
+
+    def draw_message(self, message, coord = (100, 100), preset = None, font = FONT, font_size = FONTSIZE, text_color = (160, 190, 255), background_color = None):
+        if preset == 'help':
+            background_color = (0, 0, 0)
+            text_color = (255, 255, 255)
+        if preset == 'bottom':
+            background_color = None
+        self.screen.blit(pygame.font.SysFont(font, font_size).render(message, True, text_color, background_color), coord)
